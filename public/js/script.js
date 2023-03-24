25ba6139df6c596d19b8e83d21929797558ebf36
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-03-24 11:11:09 +0100

    Initial setup and commit!

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("bytes-and-dungeons JS imported successfully!");
+});
